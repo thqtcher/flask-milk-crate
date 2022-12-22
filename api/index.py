e47290c45d6ea995252071a787fd7c6b40413da2
thqtcher <61435118+thqtcher@users.noreply.github.com>
2022-12-21 19:57:31 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
+import mc_recommendations as mc_recommendations
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def runApp():
+    results = mc_recommendations.give_me_recs()
+    df_json = results.to_json()
+    return df_json
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == 'main':
+    app.run()
