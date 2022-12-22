f4d9a76275b97184ef6b0e1d2357bd63c8fe13ee
Thatcher <thcraig@davidson.edu>
2022-12-21 21:04:34 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def runApp():
     results = give_me_recs()
     df_json = results.to_json()
-    return df_json
+    df_html = results.to_html()
+    return df_html, df_json
 
 if __name__ == 'main':
     app.run()
