cf0479ab2ed37f47536cfeb3c61978feee88e94a
thqtcher <61435118+thqtcher@users.noreply.github.com>
2022-12-23 02:00:13 -0500

    Update mc_recommendations.py

--- music/mc_recommendations.py
+++ music/mc_recommendations.py
@@ -43,8 +43,8 @@ def give_me_recs(playlist_id):
     # shuffle track ids to combat mass adding songs from same artist/album
     random.shuffle(track_ids)
     
-    if len(track_ids) > 150:
-        random.sample(track_ids, 150)
+    if len(track_ids) > 140:
+        random.sample(track_ids, 140)
     # parse through track IDs and generate recommendations on each 5-song slice
     recommended_track_list = []
 
