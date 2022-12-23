b8977c2248618ab66fe540b4d96c6858e4c7c1f5
Thatcher <thcraig@davidson.edu>
2022-12-23 01:03:03 -0500

    flippen flip
    flip

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
-import mc_recommendations as mc_recommendations
-from flask import Flask, render_template, request, redirect
+import music.mc_recommendations as mc_recommendations
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 #revert
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def show_form():
     
     return render_template('form.html')
+<<<<<<< HEAD
 """    
 @app.route('/login')
 def login():
@@ -16,17 +17,20 @@ def login():
     authorization_url = f'https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}'
     return redirect(authorization_url)
 """
+=======
+    
+
+
+>>>>>>> parent of 2be3623... Update index.py
 
     
+
+
 @app.route('/process', methods=['POST'])
 def process_input():
     input_value = request.form['input']
-
-    # Split the URL by '/' and get the last part
     parts = input_value.split('/')
     playlist_id = parts[-1]
-
-# Split the playlist ID by '?' and get the first part
     parts = playlist_id.split('?')
     playlist_id = parts[0]
 
